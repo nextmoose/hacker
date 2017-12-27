953a9778b44c2afeac8b7976a7d8e5ea2f28cbfb
Emory Merryman <emory.merryman@gmail.com>
2017-12-27 10:46:11 +0000

    must copy ssh config

--- root/run.user.sh
+++ root/run.user.sh
@@ -2,5 +2,6 @@
 
 mkdir /home/user/.ssh &&
     chmod 0700 /home/user/.ssh &&
+    cp /opt/docker/extension/config /home/user/.ssh/config &&
     touch /home/user/.ssh/volumes-backup-id-rsa &&
     chmod 0600 /home/user/.ssh/config /home/user/.ssh/volumes-backup-id-rsa
