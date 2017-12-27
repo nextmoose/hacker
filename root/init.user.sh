4915f819466ad834f234fb52a83d9ac2f1914cfe
Emory Merryman <emory.merryman@gmail.com>
2017-12-27 13:25:06 +0000

    use 5 months

--- root/init.user.sh
+++ root/init.user.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "${VOLUMES_BACKUP_PRIVATE_KEY}" > /home/user/.ssh/volumes-backup-id-rsa &&
-    echo "export AGE_DELTA=$((60*60*24*7*13*2))" >> /home/user/.bashrc
+    echo "export AGE_DELTA=$((60*60*24*7*4*5))" >> /home/user/.bashrc
