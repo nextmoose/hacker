36cc5ef6ae4e085ce999bd95e3086f0186f0dd73
Emory Merryman <emory.merryman@gmail.com>
2017-12-27 14:15:04 +0000

    finally

--- root/bin/find-empty-volumes.sh
+++ root/bin/find-empty-volumes.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-docker volume ls --filter dangling=true --quiet | while read VOLUME
+for VOLUME in $(docker volume ls --filter dangling=true --quiet)
 do
-    if [ -z "$(docker container run --interactive --rm --label expiry=$(($(date +%s)+60*60*24*7)) --volume ${VOLUME}:/volume:ro alpine:3.4 find /volume -mindepth 1)" ]
-    then
+    [ -z "$(docker container run --interactive --rm --label expiry=$(($(date +%s)+60*60*24*7)) --volume ${VOLUME}:/volume:ro alpine:3.4 find /volume -mindepth 1)" ] &&
         echo ${VOLUME}
-    fi
 done
