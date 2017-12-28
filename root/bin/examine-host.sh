36aceb27a21d5bb8c6ad2c84a7d5df83b12594d0
Emory Merryman <emory.merryman@gmail.com>
2017-12-28 21:49:22 +0000

    add examine host script

---
+++ root/bin/examine-host.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker container run --interactive --tty --rm --mount type=bind,source=/,destination=/srv,readonly=true --workdir /srv --label expiry=$(($(date)+60*60*24*7)) fedora:27 bash
