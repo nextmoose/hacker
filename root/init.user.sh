8e632f373910543ab14092e587a2bc51442a9a91
Emory Merryman <emory.merryman@gmail.com>
2017-12-28 15:14:05 +0000

    add lieutenant private key

--- root/init.user.sh
+++ root/init.user.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-echo "${VOLUMES_BACKUP_PRIVATE_KEY}" > /home/user/.ssh/volumes-backup-id-rsa &&
+echo "${HACKER_2_LIEUTENANT_PRIVATE_KEY}" > /home/user/.ssh/lieutenant_id_rsa &&
+    echo "${VOLUMES_BACKUP_PRIVATE_KEY}" > /home/user/.ssh/volumes-backup-id-rsa &&
     echo "export AGE_DELTA=$((60*60*24*7*4*5))" >> /home/user/.bashrc
