7e0e6004406f52ef43ff7f9fe40874aa9bfb9bd4
Emory Merryman <emory.merryman@gmail.com>
2017-12-28 16:44:17 +0000

    fixed error in init script

--- root/run.user.sh
+++ root/run.user.sh
@@ -2,7 +2,7 @@
 
 mkdir /home/user/.ssh &&
     chmod 0700 /home/user/.ssh &&
-    touch /home/user/lieutenant_id_rsa &&
+    touch /home/user/.ssh/lieutenant_id_rsa &&
     cp /opt/docker/extension/config /home/user/.ssh/config &&
     touch /home/user/.ssh/volumes-backup-id-rsa &&
     chmod 0600 /home/user/.ssh/config /home/user/.ssh/lieutenant_id_rsa /home/user/.ssh/volumes-backup-id-rsa
