0b120e7e7f14527c8d92e7a90cf2002a637a036d
Emory Merryman <emory.merryman@gmail.com>
2017-12-29 16:28:28 +0000

    fix it

--- root/run.user.sh
+++ root/run.user.sh
@@ -6,6 +6,6 @@ pip install awscli --upgrade --user &&
     chmod 0700 /home/user/.ssh &&
     touch /home/user/.ssh/known_hosts &&
     chmod 0644 /home/user/.ssh/known_hosts &&
-    touch /home/user/.ssh/{lieutenant.id_rsa,pavillion.id_rsa} &&
     cp /opt/docker/extension/config /home/user/.ssh/config &&
-    chmod 0600 /home/user/{config,lieutenant.id_rsa,pavillion.id_rsa}
+    touch /home/user/.ssh/{lieutenant.id_rsa,pavillion.id_rsa} &&
+    chmod 0600 /home/user/.ssh{config,lieutenant.id_rsa,pavillion.id_rsa}
