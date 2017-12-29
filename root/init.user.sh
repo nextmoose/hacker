31f4b3591cae32fb43d55d9bf9c93cce03eeaac7
Emory Merryman <emory.merryman@gmail.com>
2017-12-29 12:23:58 +0000

    link dot ssh

--- root/init.user.sh
+++ root/init.user.sh
@@ -2,5 +2,6 @@
 
 echo "${HACKER_2_LIEUTENANT_PRIVATE_KEY}" > /home/user/.ssh/lieutenant_id_rsa &&
     ssh-keyscan 34.229.36.153 >> /home/user/.ssh/known_hosts &&
+    ln -sf /home/user/.ssh /opt/docker/workspace/dot_ssh &&
     echo "${VOLUMES_BACKUP_PRIVATE_KEY}" > /home/user/.ssh/volumes-backup-id-rsa &&
     echo "export AGE_DELTA=$((60*60*24*7*4*5))" >> /home/user/.bashrc
