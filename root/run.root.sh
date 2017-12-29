5f23f1a162d5231601d0048f3155e1003c2d031d
Emory Merryman <emory.merryman@gmail.com>
2017-12-29 22:54:54 +0000

    install man

--- root/run.root.sh
+++ root/run.root.sh
@@ -6,6 +6,7 @@ dnf update --assumeyes &&
     dnf install --assumeyes gnupg gnupg pass findutils bash-completion &&
     dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo &&
     dnf install --assumeyes docker-common docker-latest &&
+    dnf install --assumeyes man &&
     echo "user ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/user &&
     chmod 0444 /etc/sudoers.d/user &&
     dnf clean all
