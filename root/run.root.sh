766f64ffa893a32899f963fc98032cf37cfbf7e0
Emory Merryman <emory.merryman@gmail.com>
2017-12-30 00:13:42 +0000

    install paperkey

--- root/run.root.sh
+++ root/run.root.sh
@@ -7,6 +7,7 @@ dnf update --assumeyes &&
     dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo &&
     dnf install --assumeyes docker-common docker-latest &&
     dnf install --assumeyes man &&
+    dnf install --assumeyes paperkey &&
     echo "user ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/user &&
     chmod 0444 /etc/sudoers.d/user &&
     dnf clean all
