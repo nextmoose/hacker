71f7b8e89fc240b433bbb7af25cf0efe64411d68
Emory Merryman <emory.merryman@gmail.com>
2017-12-30 13:31:28 +0000

    install a2ps

--- root/run.root.sh
+++ root/run.root.sh
@@ -7,7 +7,7 @@ dnf update --assumeyes &&
     dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo &&
     dnf install --assumeyes docker-common docker-latest &&
     dnf install --assumeyes man &&
-    dnf install --assumeyes paperkey &&
+    dnf install --assumeyes paperkey a2ps &&
     echo "user ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/user &&
     chmod 0444 /etc/sudoers.d/user &&
     dnf clean all
