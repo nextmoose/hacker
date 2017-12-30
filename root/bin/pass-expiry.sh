3f3160c6c86eb8e114ed3526914ac2e2f180e64e
Emory Merryman <emory.merryman@gmail.com>
2017-12-30 13:03:20 +0000

    arbitrarily set a cutoff for one week

--- root/bin/pass-expiry.sh
+++ root/bin/pass-expiry.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-pass git ls-tree -r --name-only HEAD | grep ".gpg\\\$" | while read FILE
-do
-    pass git log -1 --format="%at ${FILE}" -- ${FILE}
-done | sort -k 1
+CUTOFF=$(($(date +%s)-60*60*24*7)) &&
+    pass git ls-tree -r --name-only HEAD | grep ".gpg\$" | while read FILE
+    do
+        [ $(pass git log -1 --format=%at -- ${FILE}) -lt ${CUTOFF} ] &&
+            pass git log -1 --format="%at ${FILE}" -- ${FILE}
+    done | sort -k 1
