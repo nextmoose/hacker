f4374e15720c0f6d88193c9fc5c672816d1207d0
Emory Merryman <emory.merryman@gmail.com>
2017-12-31 00:26:50 +0000

    fuck it

--- root/bin/create-lieutenant.sh
+++ root/bin/create-lieutenant.sh
@@ -8,10 +8,10 @@ aws \
         ec2 \
         run-instances \
         --image-id ami-f0ea638a \
-        --security-group-ids $(aws ec2 create-security-group --group-name ${VOLUMES_BACKUP_SECURITY_GROUP} --description "security group for the volumes backup environment in EC2" --query "GroupId" --output text) \
+        --security-group-ids $(aws ec2 create-security-group --group-name $(uuidgen) --description "security group for the volumes backup environment in EC2" --query "GroupId" --output text) \
         --count 1 \
         --instance-type t2.micro \
-        --key-name $(aws ec2 import-key-pair --key-name ${VOLUMES_BACKUP_KEYPAIR} --public-key-material "${VOLUMES_BACKUP_PUBLIC_KEY}" --query "KeyName" --output text) \
+        --key-name $(aws ec2 import-key-pair --key-name $(uuidgen) --public-key-material "${VOLUMES_BACKUP_PUBLIC_KEY}" --query "KeyName" --output text) \
         --tag-specifications "ResourceType=instance,Tags=[{Key=moniker,Value=lieutenant}]" \
         --query "Instances[0].InstanceId" \
         --output text)
