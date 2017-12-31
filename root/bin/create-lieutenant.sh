55f69840bec52b56dc521017012dd20e76fb5ce4
Emory Merryman <emory.merryman@gmail.com>
2017-12-31 00:30:59 +0000

    wtf

--- root/bin/create-lieutenant.sh
+++ root/bin/create-lieutenant.sh
@@ -7,11 +7,11 @@ aws \
     --instance-ids $(aws \
         ec2 \
         run-instances \
-        --image-id ami-f0ea638a \
-        --security-group-ids $(aws ec2 create-security-group --group-name $(uuidgen) --description "security group for the volumes backup environment in EC2" --query "GroupId" --output text) \
+        --image-id ami-55ef662f \
+        --security-group-ids $(aws ec2 create-security-group --group-name $(uuidgen) --description "security group for the lieutenant environment in EC2" --query "GroupId" --output text) \
         --count 1 \
         --instance-type t2.micro \
-        --key-name $(aws ec2 import-key-pair --key-name $(uuidgen) --public-key-material "${VOLUMES_BACKUP_PUBLIC_KEY}" --query "KeyName" --output text) \
+        --key-name $(aws ec2 import-key-pair --key-name $(uuidgen) --public-key-material "${LIEUTENANT_AWS_PUBLIC_KEY}" --query "KeyName" --output text) \
         --tag-specifications "ResourceType=instance,Tags=[{Key=moniker,Value=lieutenant}]" \
         --query "Instances[0].InstanceId" \
         --output text)
