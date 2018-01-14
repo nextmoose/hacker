994f61f7a0d3538f9a35ac35a11420fa6ba070d3
Emory Merryman <>
2018-01-14 21:42:26 +0000

    adding disposal scripts

--- root/bin/dispose-expired-images.sh
+++ root/bin/dispose-expired-images.sh
@@ -4,6 +4,6 @@ sudo /usr/bin/docker image ls --quiet --filter dangling=true --label=expiry | wh
 do
     if [ -z "$(sudo /usr/bin/docker image inspect --format \"{{.Config.Labels.expiry}}\" ${IMAGE})" ] || [ $(sudo /usr/bin/docker image inspect --format "{{.Configs.Labels.expiry}}" ${IMAGE}) -lt $(date +%s) ]
     then
-        sudo /usr/bin/docker/image rm ${IMAGE}
+        sudo /usr/bin/docker image rm ${IMAGE}
     fi
 done
