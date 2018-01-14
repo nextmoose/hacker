e1cfb82beddbce8bc3af7fe7347f0d242ad0687a
Emory Merryman <>
2018-01-14 18:11:03 +0000

    fixing it

--- root/bin/launch-ide-github.sh
+++ root/bin/launch-ide-github.sh
@@ -24,7 +24,6 @@ export PROJECT_NAME=github &&
     cd $(mktemp -d /srv/docker/workspace/XXXXXXXX) &&
     CIDFILE=$(generate-container-id) &&
     export PROJECT_NAME=hacker4 &&
-    rm ${CIDFILE} &&
     docker \
         container \
         create \
