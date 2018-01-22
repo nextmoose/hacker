53ca1c71dc88c3bf2cac09cc91a58f42f1db46e2
Emory Merryman <>
2018-01-22 01:58:10 +0000

    fix name

--- root/cleanup.sh
+++ root/cleanup.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-ls -1 /srv/ids/containers | while read FILE
+ls -1 /srv/dockers/containers | while read FILE
 do
-    sudo /usr/bin/docker container stop $(cat /srv/ids/containers/${FILE}) &&
-        sudo /usr/bin/docker container rm --volumes $(cat /srv/ids/containers/${FILE}) &&
-        rm -f /srv/ids/${FILE}
+    sudo /usr/bin/docker container stop $(cat /srv/dockers/containers/${FILE}) &&
+        sudo /usr/bin/docker container rm --volumes $(cat /srv/dockers/containers/${FILE}) &&
+        rm -f /srv/dockers/${FILE}
 done &&
-    ls -1 /srv/ids/images | while read FILE
+    ls -1 /srv/dockers/images | while read FILE
     do
-        sudo /usr/bin/docker image rm $(cat /srv/ids/images/${FILE}) &&
-            rm -f /srv/ids/${FILE}
+        sudo /usr/bin/docker image rm $(cat /srv/dockers/images/${FILE}) &&
+            rm -f /srv/dockers/${FILE}
     done &&
-    ls -1 /srv/ids/networks | while read FILE
+    ls -1 /srv/dockers/networks | while read FILE
     do
-        sudo /usr/bin/docker network rm $(cat /srv/ids/networks/${FILE}) &&
-            rm -f /srv/ids/${FILE}
+        sudo /usr/bin/docker network rm $(cat /srv/dockers/networks/${FILE}) &&
+            rm -f /srv/dockers/${FILE}
     done &&
-    ls -1 /srv/ids/volumes | while read FILE
+    ls -1 /srv/dockers/volumes | while read FILE
     do
-        sudo /usr/bin/docker volume rm $(cat /srv/ids/volumes/${FILE}) &&
-            rm -f /srv/ids/${FILE}
+        sudo /usr/bin/docker volume rm $(cat /srv/dockers/volumes/${FILE}) &&
+            rm -f /srv/dockers/${FILE}
     done
         
