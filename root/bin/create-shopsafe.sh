6691886e236680f3f4e02c764940b9574829eec5
Emory Merryman <emory.merryman@gmail.com>
2018-01-28 23:15:32 +0000

    shopsafe

--- root/bin/create-shopsafe.sh
+++ root/bin/create-shopsafe.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-docker container create --label expiry=$(($(date +%s)+60*60*24*7)) --label project=7dccda58-5192-4c1c-8e03-75769a86df76 --volume ${VOLUME}:/home rebelplutonium/shopsafe:0.0.5
+docker \
+    container \
+    create \
+    --label expiry=$(($(date +%s)+60*60*24*7)) \
+    --label project=7dccda58-5192-4c1c-8e03-75769a86df76 \
+    --volume /tmp/.x11-unix/X0:/tmp/.x11-unix:X0:ro \
+    --env DISPLAY \
+    rebelplutonium/shopsafe:0.0.5
