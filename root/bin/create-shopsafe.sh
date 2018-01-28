7948b27414b2f0a2dbb6fd14794ad61024413674
Emory Merryman <emory.merryman@gmail.com>
2018-01-28 23:16:15 +0000

    fix case

--- root/bin/create-shopsafe.sh
+++ root/bin/create-shopsafe.sh
@@ -5,6 +5,6 @@ docker \
     create \
     --label expiry=$(($(date +%s)+60*60*24*7)) \
     --label project=7dccda58-5192-4c1c-8e03-75769a86df76 \
-    --volume /tmp/.x11-unix/X0:/tmp/.x11-unix:X0:ro \
+    --volume /tmp/.X101-unix/X0:/tmp/.X11-unix:X0:ro \
     --env DISPLAY \
     rebelplutonium/shopsafe:0.0.5
