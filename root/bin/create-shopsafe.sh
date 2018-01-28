df63e93a92a2895920f75e726ff34e49c3f7b53c
Emory Merryman <emory.merryman@gmail.com>
2018-01-28 23:24:40 +0000

    testing

--- root/bin/create-shopsafe.sh
+++ root/bin/create-shopsafe.sh
@@ -3,7 +3,7 @@
 docker \
     container \
     create \
-    --label expiry=$(($(date +%s)+60*60*24*7)) \
+    --label expiry=$(($(date +%s)+60*60*24*7*4)) \
     --label project=7dccda58-5192-4c1c-8e03-75769a86df76 \
     --volume /tmp/.X101-unix/X0:/tmp/.X11-unix-X0:ro \
     --env DISPLAY \
